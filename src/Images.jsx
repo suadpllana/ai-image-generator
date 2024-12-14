fb8645115b026cdbb90c58bc8cc0460b21a6a792
oltapllana <oltapllana@gmail.com>
2024-12-14 14:47:22 +0100

    first commit

--- src/Images.jsx
+++ src/Images.jsx
@@ -1,17 +1,23 @@
 import React from "react";
 import {useState,useRef} from "react"
 import image from "./assets/image.png"
-
+import { FaCity } from "react-icons/fa6";
+import { MdForest } from "react-icons/md";
+import { IoIosAirplane } from "react-icons/io";
 
 const Images = () => {
 
-    const inputRef = useRef(null)
 
+  const imageRef = useRef(null); 
+
+  
+  
+  const [inputText , setInputText] = useState("")
     const [imagesData , setImageData] = useState(null)
     const [loading , setLoading] = useState(false)
 
   async function getImages() {
-    if(inputRef.current.value === ""){
+    if(inputText === ""){
       alert("Please fill the input")
       return
     }
@@ -26,7 +32,7 @@ const Images = () => {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        inputs: inputRef.current.value
+        inputs: inputText
       })
     };
     
@@ -37,6 +43,7 @@ const Images = () => {
       
       setImageData(result.url)
       setLoading(false)
+      setInputText("")
     }
      
     catch (error) {
@@ -50,18 +57,53 @@ const Images = () => {
     }
   }
 
+  const prompts = [
+    {
+      icon: <FaCity className="icon"/>,
+      text:  "A futuristic cityscape at sunset, blending neon cyberpunk aesthetics with lush green vertical gardens on skyscrapers."
+    },
+    {
+      icon:  <MdForest className="icon"/>,
+      text: "A magical forest where glowing mushrooms light the way, and a small, enchanted river flows with sparkling liquid gold."
+    },
+    {
+      icon: <IoIosAirplane className="icon"/>,
+      text: "A steampunk-inspired airship flying over a Victorian-era city, with intricate gears and pipes visible against a vibrant, cloudy sky."
+    }
+   
+  ]
+
 
   return (
         <div>
+          <h2>Suggested prompts</h2>
+          <div className="promptContainer">
+         {prompts.map((item ,index) => (
+          <div key={index} onClick={() => setInputText(item.text)}>
+            {item.icon} <br/>
+            <p >{item.text}</p>
+          </div>
+         ))}
+          </div>
+
+
             <h1>AI Image <span>Generator</span></h1>
-                <img src={imagesData ? imagesData : image} alt="" /><br />
+      
+            
+            <img 
+             className="generatedImage"    
+          
+             src={imagesData ? imagesData : image} 
+             alt="" /><br />
+      
+             
                 <div className="loading">
                   <div className={loading? "loading-bar-full" : "loading-bar"}></div>
                   <div className={loading?"loading-text" :  "display-none"}>Loading...</div>
                 </div>
 
 
-                <input onKeyDown={(e) => enter(e)} placeholder="Describe what u wanna see" ref={inputRef} type="text" />
+                <input onKeyDown={(e) => enter(e)} placeholder="Describe what u wanna see" onChange={(e) => setInputText(e.target.value) } value={inputText} type="text" />
 
                 <button onClick={getImages}>Generate</button>
         </div>
